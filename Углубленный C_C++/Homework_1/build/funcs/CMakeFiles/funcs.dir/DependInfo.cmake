5e8167f70ae054983fffb8bf5f4920e104812135
Fedor Safonov <safonovfedya696@gmail.com>
2022-03-18 15:10:57 +0300

    2 CMakeLists.txt except 1

---
+++ Углубленный C_C++/Homework_1/build/funcs/CMakeFiles/funcs.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/fedor/Future/VK/Углубленный C_C++/Homework_1/funcs/animal/animal.c" "/home/fedor/Future/VK/Углубленный C_C++/Homework_1/build/funcs/CMakeFiles/funcs.dir/animal/animal.c.o"
+  "/home/fedor/Future/VK/Углубленный C_C++/Homework_1/funcs/save_input/save_input.c" "/home/fedor/Future/VK/Углубленный C_C++/Homework_1/build/funcs/CMakeFiles/funcs.dir/save_input/save_input.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../funcs/CMakeLists.txt"
+  "../funcs/animal"
+  "../funcs/constants"
+  "../funcs/save_input"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Углубленный C_C++/Homework_1/build/funcs/CMakeFiles/funcs.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/funcs.dir/animal/animal.c.o"
+  "CMakeFiles/funcs.dir/save_input/save_input.c.o"
+  "libfuncs.a"
+  "libfuncs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/funcs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
